674cce48d9ebdf3e248d9f18b2ee90676fde5f30
franastor <morales991@gmail.com>
2025-03-05 13:53:10 +0100

    fix: corregida ruta del script serve en PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 export default {
   apps: [{
     name: 'front-server',
-    script: 'serve',
+    script: './node_modules/.bin/serve',
     env: {
       PM2_SERVE_PATH: './dist',
       PM2_SERVE_PORT: 3000,
